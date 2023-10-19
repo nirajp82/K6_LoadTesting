b286020536108ca7859ecca05a9a9cc36de6c1ab
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2023-10-19 14:59:08 -0400

    Update sharedMemoryManager.js

--- Setup/sharedMemoryManager.js
+++ Setup/sharedMemoryManager.js
@@ -7,7 +7,12 @@ const arraySize = 10;
 const initialValue = 0;
 let initialized = false;
 
-if (!initialized) {
+//To avoid race conditions and ensure that shared resources are initialized correctly, use the __VU and __ITER built-in variables provided by K6. __VU represents the current Virtual User number,
+//and __ITER represents the current iteration for that Virtual User.
+
+//initialized will be set to true only for the first VU (where __VU === 1) during the first iteration (__ITER === 0).
+//This ensures that the initialization logic runs only once for the first VU during the first iteration, preventing race conditions.
+if (!initialized && __VU === 1 && __ITER === 0) {
     sharedArray[sharedArrayName] = new Array(arraySize).fill(initialValue);
     initialized = true;
 }
